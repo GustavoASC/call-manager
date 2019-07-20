a0f64bae497784b2c6b88ed65798eaa2b7dc9669
Gustavo Cassel <guscassel@gmail.com>
2019-07-20 16:19:25 -0300

    Created first class

---
+++ src/main/java/cassel/call/manager/Starter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cassel.call.manager;
+
+/**
+ *
+ * @author gustavo
+ */
+public enum Starter {
+    
+}
